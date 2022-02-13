f04fc3e9aa9707e88113bffa7cf14ca7e77ad31d
mtnmr <mei.rila1013@gmail.com>
2022-02-13 16:28:01 +0900

    BindingAdapter add

---
+++ mvvmsample/app/src/main/java/com/example/mvvmsample/BindingAdapters.kt
@@ -0,0 +1,30 @@
+package com.example.mvvmsample
+
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.databinding.BindingAdapter
+
+object BindingAdapters {
+
+    @JvmStatic
+    @BindingAdapter("app:goneUnless")
+    fun goneUnless(view: View, visible:Boolean){
+        view.visibility = if (visible) View.VISIBLE else View.GONE
+    }
+
+    @JvmStatic
+    @BindingAdapter("app:iconId")
+    fun setIconById(view:AppCompatImageView,iconId:Int){
+        val resourceId = when (iconId){
+            1 -> R.drawable.avatar_01
+            2 -> R.drawable.avatar_02
+            3 -> R.drawable.avatar_03
+            4 -> R.drawable.avatar_04
+            else -> null
+        }
+
+        resourceId?.let {
+            view.setImageResource(it)
+        } ?: view.setImageDrawable(null)
+    }
+}
